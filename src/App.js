85bfb3766f7c69936fa92b4eb31e85fa18893114
yassine-sys <ymezghani@gmail.com>
2021-04-01 13:29:02 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav class="navbar navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+</nav>
+      <form class="row g-3">
+        <div class="col-md-12">
+          <label for="validationServer01" class="form-label">First name</label>
+          <input type="text" class="form-control is-valid" id="validationServer01" value="Mark" required></input>
+
+
+        </div>
+        <br></br>
+
+        <div class="col-md-12">
+          <label for="validationServer01" class="form-label">Last name</label>
+          <input type="text" class="form-control is-valid" id="validationServer01" value="Mark" required></input>
+          
+
+        </div>
+
+        <div class="col-md-12">
+          <label for="validationServer01" class="form-label">Username</label>
+          <input type="text" class="form-control is-valid" id="validationServer01" value="Mark" required></input>
+          
+
+        </div>
+
+        <div class="container">
+  <div class="row">
+    <div class="col text-center">
+      <button class="btn btn-primary btn-lg btn-block">Submit</button>
     </div>
+  </div>
+</div>
+     
+        
+      </form>
+    
+    </div>
+    
   );
 }
 
